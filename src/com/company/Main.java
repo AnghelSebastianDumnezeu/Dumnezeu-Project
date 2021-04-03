fd88eb1c8098067e72333bfd1273e3c8ffb0a23c
Sebastian <colindefaine@yahoo.com>
2021-04-03 11:47:50 +0300

    Proiect Functii

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,32 @@ package com.company;
 
 public class Main {
 
+
+
+    public static int calculatemultiple (int numberOne, int numberTwo) {
+        return numberOne * numberTwo;
+
+    }
+
+
+
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("anghel");
-        
+    int myFirstNumber = 3;
+    int mySecondNumber = 5;
+
+    System.out.println(calculatemultiple(3,5));
+
+    public static int calculateSum (int numberOne, int numberTwo);
+    return numberOne + numberTwo;
+
+
+        }
+        public static void main(String[] args) {
+        int myFirstNumber =5;
+        int mySecondNumber = 5;
+       System.out.println (calculatemultiple( int numberOne:5, int numberTwo:5);
+       return numberOne * numberTwo;
     }
 }
+
